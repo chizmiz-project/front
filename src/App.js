05586f2b8c56541e582e041d61540e29f63d6f48
s.soltanian <s.soltaniant@gmail.com>
2024-12-10 02:13:06 +0330

    preview main layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { createTheme } from '@mui/material/styles';
 import { CacheProvider, ThemeProvider } from '@emotion/react';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { prefixer } from 'stylis';
 import createCache from '@emotion/cache';
-import { TextField } from '@mui/material';
+import MainLayout from './layouts/MainLayout';
 
 const theme = createTheme({
   direction: 'rtl',
@@ -21,12 +20,7 @@ function App() {
   return (
     <CacheProvider value={cacheRtl}>
       <ThemeProvider theme={theme}>
-      <TextField
-        label="ملصق"
-        placeholder="العنصر النائب"
-        helperText="هذا نص مساعد"
-        variant="outlined"
-      />
+        <MainLayout></MainLayout>
     </ThemeProvider>
     </CacheProvider>
   );
