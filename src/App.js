d5b50e3b1904c11f920318bec4661beda1cc8a63
s.soltanian <s.soltaniant@gmail.com>
2024-12-09 15:26:32 +0330

    add custom persian font

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          فایل <code>src/App.js</code> را ویرایش کنید 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          پروژه اولیه ریکت به زبان فارسی
         </a>
       </header>
     </div>
