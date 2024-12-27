b4e1f72ddc39dbf511adc85e8fe81d1222eda6ad
s.soltanian <s.soltaniant@gmail.com>
2024-12-27 18:10:59 +0330

    add category item component

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,27 @@
+import { Box, Paper, Typography } from '@mui/material';
+import { Home } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+
+export function CategoryItem({ title, value }) {
+  const navigate = useNavigate();
+
+  return (
+    <Paper
+      elevation={0}
+      style={{
+        padding: '16px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '8px',
+        backgroundColor: 'rgba(158, 158, 158, 0.1)',
+        cursor: 'pointer'
+      }}
+      onClick={() => navigate(`/category/${value}`)}
+    >
+      <Home />
+      <Typography variant="body2">{title}</Typography>
+    </Paper>
+  );
+}
+
