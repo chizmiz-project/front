c66be795ff9efb70d0d084bc04408a460804480c
s.soltanian <s.soltaniant@gmail.com>
2024-12-27 18:25:35 +0330

    update routings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import createCache from '@emotion/cache';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SettingsPage from './layouts/settingPage'
 import Theme from './theme.js'
+import MainPage from './layouts/MainPage.js';
+import CategoryPage from './layouts/CategoryMainPage.js';
 
 const cacheRtl = createCache({
   key: 'muirtl',
@@ -21,8 +23,8 @@ function App() {
           <Routes>
             <Route path="/settings" element={<SettingsPage />} />
             <Route path="/account/edit" element={<SettingsPage />} />
-            <Route path="/saved-ads" element={<SettingsPage />} />
-            <Route path="/" element={<SettingsPage/>} />
+            <Route path="/category/" element={<CategoryPage />} />
+            <Route path="/" element={<MainPage/>} />
             {/* <Route path="/recent-views" element={<RecentViews />} /> */}
           </Routes>
         </BrowserRouter>
