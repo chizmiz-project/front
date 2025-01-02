96ac3409a0a4d757a6b1e521f3c696531c9dfb9a
s.soltanian <s.soltaniant@gmail.com>
2025-01-02 23:02:35 +0330

    add create add page

--- src/layouts/pages/CreateAdPage.js
+++ src/layouts/pages/CreateAdPage.js
@@ -23,57 +23,67 @@ export default function CreateAdPage() {
     categoryId: "",
     title: "",
     description: "",
-    image: null
+    image: null,
+    price: ""
   })
 
   const handleSubmit = async e => {
     e.preventDefault()
-        const data = {
-        categoryId: formData.categoryId,
-        title: formData.title,
-        description: formData.description,
-        image: formData.image || undefined
-        }
+    const data = {
+      categoryId: formData.categoryId,
+      title: formData.title,
+      description: formData.description,
+      price: formData.price,
+      image: formData.image || undefined
+    }
 
-        const response = await ApiService.post('/advertisement/', data);
-        if (response.isSuccess)
-          navigate('/')
+    console.log(data)
+    const response = await ApiService.post('/advertisement/', data);
+    if (response.isSuccess)
+      navigate('/')
   }
 
   return (
     <AppLayout title="ساخت آگهی">
-        <form onSubmit={handleSubmit}>
-          <CategorySelector
-            categories={categories}
-            selectedCategory={formData.categoryId}
-            onSelect={categoryId => setFormData({ ...formData, categoryId })}
-          />
+      <form onSubmit={handleSubmit}>
+        <CategorySelector
+          categories={categories}
+          selectedCategory={formData.categoryId}
+          onSelect={categoryId => setFormData({ ...formData, categoryId })}
+        />
 
-          <CustomTextField
-            title="عنوان فیلد"
-            description="توضیحات بیشتر درباره فیلدی که قرار است پر شود"
-            placeholder="متن را وارد کنید"
-            value={formData.title}
-            onChange={title => setFormData({ ...formData, title })}
-          />
+        <CustomTextField
+          title="عنوان فیلد"
+          description="توضیحات بیشتر درباره فیلدی که قرار است پر شود"
+          placeholder="متن را وارد کنید"
+          value={formData.title}
+          onChange={title => setFormData({ ...formData, title })}
+        />
 
-          <CustomTextField
-            title="توضیحات"
-            description="توضیحات بیشتر درباره فیلدی که قرار است پر شود"
-            placeholder="متن را وارد کنید"
-            multiline
-            value={formData.description}
-            onChange={description => setFormData({ ...formData, description })}
-          />
+        <CustomTextField
+          title="توضیحات"
+          description="توضیحات بیشتر درباره فیلدی که قرار است پر شود"
+          placeholder="متن را وارد کنید"
+          multiline
+          value={formData.description}
+          onChange={description => setFormData({ ...formData, description })}
+        />
 
-          <ImageUploader
-            onImageSelect={image => setFormData({ ...formData, image })}
-          />
+        <CustomTextField
+          title="قیمت"
+          placeholder="قیمت را وارد کنید"
+          value={formData.price}
+          onChange={price => setFormData({ ...formData, price })}
+        />
 
-          <Button type="submit" fullWidth variant="contained" size="large">
-            ساخت آگهی
-          </Button>
-        </form>
+        <ImageUploader
+          onImageSelect={image => setFormData({ ...formData, image })}
+        />
+
+        <Button type="submit" fullWidth variant="contained" size="large">
+          ساخت آگهی
+        </Button>
+      </form>
     </AppLayout>
   )
 }
