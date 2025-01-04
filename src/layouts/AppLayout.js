48a1a1e532e2078be45ac53bc931ed6cf99edefe
Soltaniant <47256432+Soltaniant@users.noreply.github.com>
2025-01-04 21:50:04 +0330

    Search (#16)
    * add empty space for no result of search
    
    * reduce the search effect using debounce
    
    * update layout and bar to be compatible with search requirments
    
    * update main page
    
    * add url options to get request
    
    * add search query to api call

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,10 +1,10 @@
 import { Box, Container } from "@mui/material";
 import { AppBar } from "../components/AppBar";
 
-const AppLayout = ({variant = 'title', title = '', hasNavigate = true, children}) => {
+const AppLayout = ({variant = 'title', title = '', hasNavigate = true, children, onSearchChange}) => {
     return (
         <Box minHeight={'100vh'} bgcolor={'grey.50'}>
-            <AppBar variant={variant} title={title} hasNavigate={hasNavigate}/>
+            <AppBar variant={variant} title={title} hasNavigate={hasNavigate} onSearchChange={onSearchChange}/>
 
             <Container sx={{ py: 2 }}>
                 {children}
--- src/layouts/pages/MainPage.js
+++ src/layouts/pages/MainPage.js
@@ -1,9 +1,11 @@
-import { Box, Grid } from '@mui/material'
+import { Box, CircularProgress, debounce, Grid } from '@mui/material'
 import AdItem from '../../components/advertisement/AdItem'
 import { CategoryItem } from '../../components/CategoryItem'
 import { useEffect, useState } from 'react'
 import ApiService from '../../services/api'
 import AppLayout from '../AppLayout'
+import { useDebounce } from '../../components/useDebounce'
+import EmptyState from '../../components/EmptyState'
 
 const categories = [
   { title: 'املاک', value: 'real-estate' },
@@ -15,25 +17,33 @@ const categories = [
 ]
 
 export default function MainPage() {
-
+  const [searchQuery, setSearchQuery] = useState('')
   const [ads, setAds] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const debouncedSearch = useDebounce(searchQuery, 300)
 
   useEffect(() => {
     const fetchAds = async () => {
-      const response = await ApiService.get('/advertisement/');
-      if (response.isSuccess) {
-        console.log(response.data);
-        setAds(response.data);
-      } else {
-        console.error('Fetch failed:', response);
+      setIsLoading(true)
+      try {
+        const response = await ApiService.get('/advertisement/', {
+          search: searchQuery
+        });
+        if (response.isSuccess) {
+          setAds(response.data);
+        } else {
+          console.error('Fetch failed:', response);
+        }
+      } finally {
+        setIsLoading(false)
       }
-    };
+    }
 
-    fetchAds();
-  }, []);
+    fetchAds()
+  }, [debouncedSearch])
 
   return (
-    <AppLayout variant='search' hasNavigate={false}>
+    <AppLayout variant='search' hasNavigate={false} onSearchChange={setSearchQuery}>
       <Grid container spacing={2} sx={{ mb: 3 }}>
         {categories.map((category, index) => (
           <Grid item xs={4} key={index}>
@@ -41,12 +51,20 @@ export default function MainPage() {
           </Grid>
         ))}
       </Grid>
-
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        {ads.map((ad, index) => (
-          <AdItem key={index} ad={ad} />
-        ))}
-      </Box>
+      
+      {isLoading ? (
+          <Box sx={{ display: 'flex', justifyContent: 'center', py: 4 }}>
+            <CircularProgress />
+          </Box>
+        ) : ads.length > 0 ? (
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            {ads.map((ad, index) => (
+              <AdItem key={index} ad={ad} />
+            ))}
+          </Box>
+        ) : searchQuery ? (
+          <EmptyState/>
+        ) : null}
     </AppLayout>
 
   )
