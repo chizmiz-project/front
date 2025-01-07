caf9431cf542a854178f01c6f8d825ba537cf2ca
s.soltanian <s.soltaniant@gmail.com>
2025-01-07 21:07:41 +0330

    add snackbar component provider

---
+++ src/context/SnackbarProvider.js
@@ -0,0 +1,59 @@
+import React, { createContext, useContext, useState } from 'react';
+import { Snackbar, Slide, SnackbarContent } from '@mui/material';
+import { errorColor, infoColor, successColor, warningColor } from '../theme';
+
+const SnackbarContext = createContext();
+
+export const SnackbarProvider = ({ children }) => {
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [backgroundColor, setBackgroundColor] = useState(infoColor);
+
+    const openSnackbar = (msg, state) => {
+        setMessage(msg);
+        switch (state) {
+            case 'success':
+                setBackgroundColor(successColor);
+                break;
+            case 'info':
+                setBackgroundColor(infoColor);
+                break;
+            case 'error':
+                setBackgroundColor(errorColor);
+                break;
+            case 'server':
+                setBackgroundColor(warningColor)
+                break;
+        }
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <SnackbarContext.Provider value={{ openSnackbar }}>
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                open={open}
+                autoHideDuration={1500}
+                onClose={handleClose}
+                TransitionComponent={Slide}
+            >
+                <SnackbarContent 
+                    message={message}
+                    sx={{
+                        backgroundColor: backgroundColor,
+                    }}
+                />
+            </Snackbar>
+            {children}
+        </SnackbarContext.Provider>
+    );
+};
+
+// Custom hook to use Snackbar context
+export const useSnackbar = () => {
+    return useContext(SnackbarContext);
+};
